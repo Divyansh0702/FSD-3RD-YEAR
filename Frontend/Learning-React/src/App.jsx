58ef3e52049060e437b2b61c72f9fc96c6e8174e
Divyansh0702 <divyansh07020@gmail.com>
2025-10-27 12:46:51 +0530

    Netflix Clone

--- Frontend/Learning-React/src/App.jsx
+++ Frontend/Learning-React/src/App.jsx
@@ -1,15 +1,81 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      
-    </div>
+    <>
+  
+      <div>
+        <h1>To-Do List</h1>
+        
+        <Container>
+          <Row>
+            <Col>
+
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+
+            </Col>
+
+            <Col>
+
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+
+            </Col>
+
+            <Col>
+
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+
+            </Col>
+            
+          </Row>
+
+    </Container>
+
+      </div>
+
+    </>
   )
 }
 
---
+++ React/index.js
@@ -0,0 +1,1 @@
+const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
